d9346e0bb90590061a1671d4c61c3a2339cd5374
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-05 18:00:22 -0300

    feat: add new urls

--- backend/api_rest/urls.py
+++ backend/api_rest/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_api, name='login')
+    path('login/', views.login_api, name='login'),
+    path('user/', views.get_user_data, name='user'),
+    path('register/', views.register_api, name='register')
 ]
