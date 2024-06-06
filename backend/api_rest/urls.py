1e575bc763211309dfec407df24eca1de9ca8cd8
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-06 11:48:39 -0300

    refactor: standardizing code

--- backend/api_rest/urls.py
+++ backend/api_rest/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('login/', views.login_api, name='login'),
     path('user/', views.get_user_data, name='user'),
-    path('register_patient/', views.register_cliente_api, name='register_patient'),
-    path('register_doctor/', views.register_medico_api, name='register_doctor'),
+    path('register_patient/', views.register_client_api, name='register_patient'),
+    path('register_doctor/', views.register_doctor_api, name='register_doctor'),
     path('logout/', knox_views.LogoutView.as_view()),
     path('logoutall', knox_views.LogoutAllView.as_view())
 ]
