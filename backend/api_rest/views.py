19e99bb3f06477001a83b24259e4a0348a208b44
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-06 11:47:17 -0300

    feat: adding view to doctors record

--- backend/api_rest/views.py
+++ backend/api_rest/views.py
@@ -2,11 +2,28 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.models import AuthToken
-from .serializers import RegisterSerializer
+from .serializers import RegisterClienteSerializer, RegisterMedicoSerializer
 
 @api_view(['POST'])
-def register_api(request):
-    serializer = RegisterSerializer(data=request.data)
+def register_client_api(request):
+    serializer = RegisterClienteSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    user = serializer.save()
+    _, token = AuthToken.objects.create(user)
+
+    return Response({
+        'user_info': {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email
+        },
+        'token': token
+    })
+
+@api_view(['POST'])
+def register_doctor_api(request):
+    serializer = RegisterMedicoSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
 
     user = serializer.save()
