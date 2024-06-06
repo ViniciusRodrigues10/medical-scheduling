2cfdc36a1c268920390fb0615a2c02f26af69092
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-06 11:41:43 -0300

    feat: creating model for doctors registration

--- backend/api_rest/models.py
+++ backend/api_rest/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_medico = models.BooleanField(default=False)
+    crm = models.CharField(max_length=20, blank=True, null=True)
+    specialization = models.CharField(max_length=100, blank=True, null=True)
