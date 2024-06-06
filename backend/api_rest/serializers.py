747bd256f2d404d3bd873a64150df06cac4668ae
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-06 10:32:50 -0300

    fix: solving problems when saving data

--- backend/api_rest/serializers.py
+++ backend/api_rest/serializers.py
@@ -1,37 +1,36 @@
+from rest_framework import serializers
 from django.contrib.auth.models import User
-from rest_framework import serializers, validators
+from rest_framework.validators import UniqueValidator
+import logging
+
+logger = logging.getLogger(__name__)
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password', 'email', 'first_name', 'last_name')
-
         extra_kwargs = {
             "password": {"write_only": True},
             "email": {
                 "required": True,
                 "allow_blank": False,
                 "validators": [
-                    validators.UniqueValidator(
-                        User.objects.all(), "A user with that Email already exists"
+                    UniqueValidator(
+                        queryset=User.objects.all(), message="A user with that Email already exists"
                     )
                 ]
             }
         }
-    
+
     def create(self, validated_data):
-        username = validated_data.get('username')
-        password = validated_data.get('password')
-        email = validated_data.get('email')
-        first_name = validated_data.get('first_name')
-        last_name = validated_data.get('last_name')
-
-        user = User.objects.create(
-            username = username,
-            password = password,
-            email = email,
-            first_name = first_name,
-            last_name = last_name
+        logger.info("Creating user with username: %s", validated_data['username'])
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
-
+        user.set_password(validated_data['password'])
+        logger.info("Password set for user: %s", validated_data['username'])
+        user.save()
         return user
