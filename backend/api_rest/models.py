8ed1865e18ad7173ba1f51b7557ae4e27b5b9017
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-07 16:36:33 -0300

    feat: creating table for user registration

--- backend/api_rest/models.py
+++ backend/api_rest/models.py
@@ -1,8 +1,54 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_medico = models.BooleanField(default=False)
     crm = models.CharField(max_length=20, blank=True, null=True)
     specialization = models.CharField(max_length=100, blank=True, null=True)
+
+class Availability(models.Model):
+    doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    class Meta:
+        unique_together = ('doctor', 'date', 'start_time', 'end_time')
+
+    def __str__(self):
+        return f"{self.doctor.username} - {self.date} {self.start_time} - {self.end_time}"
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=255)
+    tipo_usuario = models.CharField(max_length=20)
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    data_nascimento = models.DateField(blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
