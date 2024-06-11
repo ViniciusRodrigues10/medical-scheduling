6c5ccc52085c83351b1b613cf0927b6d09076e9d
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-11 16:23:26 -0300

    feat: adding url for patient registration

--- backend/api_rest/urls.py
+++ backend/api_rest/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
-from knox import views as knox_views
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_api, name='login'),
-    path('user/', views.get_user_data, name='user'),
-    path('register_patient/', views.register_client_api, name='register_patient'),
-    path('register_doctor/', views.register_doctor_api, name='register_doctor'),
-    path('logout/', knox_views.LogoutView.as_view()),
-    path('logoutall', knox_views.LogoutAllView.as_view())
+    path('register-patient/', views.register_patient_api, name='register-patient'),
 ]
