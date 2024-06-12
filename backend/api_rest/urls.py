983e8ac47c81cda2deeca342ce6c45dda5da5f52
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-12 17:51:53 -0300

    feat: adding endpoint for updating user data

--- backend/api_rest/urls.py
+++ backend/api_rest/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register-patient/', views.register_patient_api, name='register-patient'),
     path('login/', views.login_api, name='login'),
     path('user/', views.get_user_data, name='user'),
+    path('update-user/', views.update_user_data, name='update-user'),
 ]
  
