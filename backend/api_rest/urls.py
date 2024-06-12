6da60f6defcf2d58454e02e8f3c1aca385a8ffb3
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-12 18:03:37 -0300

    feat: adding endpoint for logout

--- backend/api_rest/urls.py
+++ backend/api_rest/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from knox import views as knox_views
 from . import views
 
 urlpatterns = [
@@ -6,5 +7,7 @@ urlpatterns = [
     path('login/', views.login_api, name='login'),
     path('user/', views.get_user_data, name='user'),
     path('update-user/', views.update_user_data, name='update-user'),
+    path('logout/', knox_views.LogoutView.as_view()),
+    path('logoutall', knox_views.LogoutAllView.as_view())
 ]
  
