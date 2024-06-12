4ee3a4818cc0b2fc3c562b3baeb8695b8e3c286e
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-12 13:00:17 -0300

    feat: adding endpoint to get user data

--- backend/api_rest/urls.py
+++ backend/api_rest/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('register-patient/', views.register_patient_api, name='register-patient'),
     path('login/', views.login_api, name='login'),
+    path('user/', views.get_user_data, name='user'),
 ]
  
