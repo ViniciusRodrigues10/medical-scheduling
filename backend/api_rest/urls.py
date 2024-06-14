676d61b02eb12e0febea61e7b1aa1d13e6fab996
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-14 09:31:02 -0300

    feat: creating endpoint to get doctor data

--- backend/api_rest/urls.py
+++ backend/api_rest/urls.py
@@ -4,12 +4,14 @@ from . import views
 
 urlpatterns = [
     path('register-patient/', views.register_patient_api, name='register-patient'),
+    path('register-doctor/', views.register_doctor, name='register-doctor'),
     path('login/', views.login_api, name='login'),
     path('user/', views.get_user_data, name='user'),
+    path('user-doctor/', views.get_doctor_data, name='user-doctor'),
     path('update-user/', views.update_user_data, name='update-user'),
     path('logout/', knox_views.LogoutView.as_view()),
     path('logoutall', knox_views.LogoutAllView.as_view()),
     path('delete-account/', views.delete_user_account, name='delete-account'),
-    path('register-doctor/', views.register_doctor, name='register-doctor'),
+    
 ]
  
