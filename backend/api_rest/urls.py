6a35b80d0273cafb386f6ac800b2dc020fdfccde
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-14 10:01:56 -0300

    feat: adding endpoint to update doctor data

--- backend/api_rest/urls.py
+++ backend/api_rest/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('user/', views.get_user_data, name='user'),
     path('user-doctor/', views.get_doctor_data, name='user-doctor'),
     path('update-user/', views.update_user_data, name='update-user'),
+    path('user-doctor/update-doctor', views.update_doctor_data, name='update-doctor'),
     path('logout/', knox_views.LogoutView.as_view()),
     path('logoutall', knox_views.LogoutAllView.as_view()),
     path('delete-account/', views.delete_user_account, name='delete-account'),
