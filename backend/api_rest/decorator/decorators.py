78ef32803d20211e4575b8540fe95a6fd11a3f0c
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-08-27 14:23:27 -0300

    feat(decorators.py): update file with decorator patterns

--- backend/api_rest/decorator/decorators.py
+++ backend/api_rest/decorator/decorators.py
@@ -1,15 +1,31 @@
 from functools import wraps
 from django.http import HttpResponseForbidden
+from rest_framework.response import Response
+from rest_framework import status
 
 
-def superuser_required(function):
+def superuser_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        print(request.user.is_superuser)
+        if request.user.is_superuser:
+            return view_func(request, *args, **kwargs)
+        return Response(
+            {
+                "detail": "Permission denied. You must be a superuser to perform this action."
+            },
+            status=status.HTTP_403_FORBIDDEN,
+        )
+
+    return _wrapped_view
+
+
+def login_required_custom(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
-        if request.user.is_superuser:
+        if request.user.is_authenticated:
             return function(request, *args, **kwargs)
         else:
-            return HttpResponseForbidden(
-                "You do not have permission to access this page."
-            )
+            return HttpResponseForbidden("Você não está logado")
 
     return wrap
