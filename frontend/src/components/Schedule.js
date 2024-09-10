7c38207392f8fd7146dd97f20c500e5873c744c4
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-09-10 15:05:12 -0300

    feat(frontend): adjust calendar screen

--- frontend/src/components/Schedule.js
+++ frontend/src/components/Schedule.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import Sidebar from './Menu'; // Importa a função da barra lateral
-import '../_assets/css/agenda.css'; // Importa o CSS
+import Sidebar from './Menu';
+import '../_assets/css/agenda.css';
 
 const Schedule = () => {
     const [appointments, setAppointments] = useState([]);
@@ -18,7 +18,7 @@ const Schedule = () => {
             }
 
             try {
-                const response = await axios.get('http://127.0.0.1:8000/api/appointments/future', {
+                const response = await axios.get('http://localhost:8000/api/user/appointments/', {
                     headers: {
                         'Authorization': `Token ${token}`
                     }
@@ -35,23 +35,23 @@ const Schedule = () => {
 
     return (
         <div className="profile-container">
-            {/* Adiciona a barra lateral importada */}
             <Sidebar />
             <div className="main-content">
                 <header className="header">
-                    <h1>Agendamentos Futuras</h1>
+                    <h1>Consultas Marcadas</h1>
                 </header>
                 <div className="appointments-container">
+                    {error && <p className="error-message">{error}</p>}
                     {appointments.length === 0 ? (
                         <p>Não há agendamentos futuros.</p>
                     ) : (
                         <ul className="appointments-list">
                             {appointments.map((appointment) => (
                                 <li key={appointment.id_appointment}>
+                                    <p className="specialty">{appointment.doctor_specialty}</p>
+                                    <p><strong>Médico:</strong> {appointment.doctor_first_name} {appointment.doctor_last_name}</p>
                                     <p><strong>Data:</strong> {appointment.date}</p>
                                     <p><strong>Hora:</strong> {appointment.start_time} - {appointment.end_time}</p>
-                                    <p><strong>Especialidade:</strong> {appointment.doctor_specialty}</p>
-                                    <p><strong>Médico:</strong> {appointment.doctor_first_name} {appointment.doctor_last_name}</p>
                                 </li>
                             ))}
                         </ul>
