cc95744656c3e4a9d7e2b1d3145fa7b868d4eb60
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-09-10 20:39:15 -0300

    feat(frontend): update schedule

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import Sidebar from './Menu'; // Importa a função da barra lateral
-import '../_assets/css/userProfile.css'; // Importa o CSS
+import Sidebar from './Menu'; 
+import '../_assets/css/userProfile.css';
 
 const UserProfile = () => {
     const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
     const navigate = useNavigate();
 
@@ -24,39 +25,44 @@ const UserProfile = () => {
                     }
                 });
                 setUserData(response.data);
+                setLoading(false);
             } catch (error) {
                 setError('Erro ao obter dados do usuário');
                 console.error(error);
+                setLoading(false);
             }
         };
 
         fetchUserData();
     }, [navigate]);
 
-    if (!userData) {
-        return <div>Carregando...</div>;
-    }
-
     return (
         <div className="profile-container">
-            {/* Adiciona a barra lateral importada */}
             <Sidebar /> 
             <div className="main-content">
                 <header className="header">
                     <h1>Perfil</h1>
                     <div className="user-info">
-                        <span>{userData.user_info.first_name} {userData.user_info.last_name}</span>
+                        <span>{userData?.user_info.first_name} {userData?.user_info.last_name}</span>
                         <img src={require('../_assets/img/icone-perfil.png')} alt="User Avatar" className="user-perfil" />
                     </div>
                 </header>
                 <div className="user-details">
-                    <h2>Meus Dados</h2>
-                    <div className="user-card">
-                        <p><strong>Nome:</strong> {userData.user_info.first_name} {userData.user_info.last_name}</p>
-                        <p><strong>Email:</strong> {userData.user_info.email}</p>
-                        <p><strong>Gênero:</strong> {userData.user_info.gender === 1 ? 'Masculino' : 'Feminino'}</p>
-                    </div>
-                    <button onClick={() => navigate('/historico-medico')} className="history-button">Preencher Histórico Médico</button>
+                    {loading ? (
+                        <p>Carregando...</p>
+                    ) : error ? (
+                        <p className="error-message">{error}</p>
+                    ) : (
+                        <>
+                            <h2>Meus Dados</h2>
+                            <div className="user-card">
+                                <p><strong>Nome:</strong> {userData.user_info.first_name} {userData.user_info.last_name}</p>
+                                <p><strong>Email:</strong> {userData.user_info.email}</p>
+                                <p><strong>Gênero:</strong> {userData.user_info.gender === 1 ? 'Masculino' : 'Feminino'}</p>
+                            </div>
+                            <button onClick={() => navigate('/historico-medico')} className="history-button">Preencher Histórico Médico</button>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
