056348bb7183725221bc9efd748e207282e46590
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-09-10 20:40:19 -0300

    feat(frontend): update menu

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../_assets/css/menu.css'; // Importando o CSS
-
-// Importando as imagens corretamente
+import { useNavigate, useLocation } from 'react-router-dom';
+import '../_assets/css/menu.css'; 
 import logo from '../_assets/img/logo.png';
 import iconHome from '../_assets/img/icone-casa.png';
 import iconDoctors from '../_assets/img/icone-perfil.png';
@@ -11,31 +9,35 @@ import iconReports from '../_assets/img/icone-estati.png';
 import iconSettings from '../_assets/img/icone-opcao.png';
 
 const Menu = () => {
-    const navigate = useNavigate(); // Inicializa o hook de navegação
-  
+    const navigate = useNavigate();
+    const location = useLocation(); // Hook para obter a localização atual
+
+    // Função para determinar se o item do menu está ativo
+    const isActive = (path) => location.pathname === path;
+
     return (
         <div className="sidebar">
             <img src={logo} alt="Saúde Now" className="sidebar-logo" />
             <ul className="menu-list">
-                <li className="menu-item" onClick={() => navigate('/home')}>
+                <li className={`menu-item ${isActive('/login') ? 'active' : ''}`} onClick={() => navigate('/login')}>
                     <img src={iconHome} alt="Ícone de Início" className="icon" />
                     Início
                 </li>
-                <li className="menu-item" onClick={() => navigate('/medicos')}>
+                <li className={`menu-item ${isActive('/medicos') ? 'active' : ''}`} onClick={() => navigate('/medicos')}>
                     <img src={iconDoctors} alt="Ícone de Médicos" className="icon" />
                     Médicos
                 </li>
-                <li className="menu-item" onClick={() => navigate('/agenda')}>
+                <li className={`menu-item ${isActive('/agenda') ? 'active' : ''}`} onClick={() => navigate('/agenda')}>
                     <img src={iconCalendar} alt="Ícone de Agenda" className="icon" />
                     Agenda
                 </li>
-                <li className="menu-item" onClick={() => navigate('/relatorios')}>
+                <li className={`menu-item ${isActive('/login/historico-de-agendamentos') ? 'active' : ''}`} onClick={() => navigate('/login/historico-de-agendamentos')}>
                     <img src={iconReports} alt="Ícone de Relatórios" className="icon" />
-                    Relatórios
+                    Histórico
                 </li>
-                <li className="menu-item" onClick={() => navigate('/opcoes')}>
+                <li className={`menu-item ${isActive('/perfil') ? 'active' : ''}`} onClick={() => navigate('/perfil')}>
                     <img src={iconSettings} alt="Ícone de Opções" className="icon" />
-                    Opções
+                    Perfil
                 </li>
             </ul>
             <button onClick={() => navigate('/login')} className="logout-button">Sair</button>
